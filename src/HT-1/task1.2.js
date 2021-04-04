60f7b68b72a6c1a7dd209b8b8c8b704e00b6cd60
sahil1504001 <sahilshikalgar001@gmail.com>
2021-04-04 13:57:54 +0530

    feat(main): Task 1.1 & 1.2

---
+++ src/HT-1/task1.2.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+const Converter = require("csvtojson").Converter;
+const csvFilePath = 'src/assets/nodejs-hw1-ex1.csv';
+
+const csvConverter = new Converter({ constructResult: false, toArrayString: true });
+const readStream = fs.createReadStream(csvFilePath); 
+const writeStream = fs.createWriteStream("nodejs19-hw1-ex2.txt");
+
+readStream.pipe(csvConverter).pipe(writeStream);
