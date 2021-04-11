54a8e52554b45e8b8841023e2468cfb026d5ce94
sahil1504001 <sahilshikalgar001@gmail.com>
2021-04-11 13:57:35 +0530

    eslint setup and error fix

--- src/HT-1/task1.1.js
+++ src/HT-1/task1.1.js
@@ -1,14 +1,14 @@
 const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout
 });
 
 const reverseString = () => {
-  readline.question('', (input) => {
-    readline.write([...input].reverse().join(''));
-    readline.write('\n\n\n');
-    reverseString();
-  });
+    readline.question('', (input) => {
+        readline.write([...input].reverse().join(''));
+        readline.write('\n\n\n');
+        reverseString();
+    });
 };
 
 reverseString();
