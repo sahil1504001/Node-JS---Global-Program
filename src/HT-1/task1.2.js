0b628efd569b48ee6eded30de0ea97a82733e06d
sahil1504001 <sahilshikalgar001@gmail.com>
2021-04-11 13:48:16 +0530

    eslint setup and error fix

--- src/HT-1/task1.2.js
+++ src/HT-1/task1.2.js
@@ -1,9 +1,10 @@
-const fs = require("fs")
-const Converter = require("csvtojson").Converter;
+const fs = require('fs');
+const { Converter } = require('csvtojson');
+
 const csvFilePath = 'src/assets/nodejs-hw1-ex1.csv';
 
 const csvConverter = new Converter({ constructResult: false, toArrayString: true });
-const readStream = fs.createReadStream(csvFilePath); 
-const writeStream = fs.createWriteStream("nodejs19-hw1-ex2.txt");
+const readStream = fs.createReadStream(csvFilePath);
+const writeStream = fs.createWriteStream('nodejs19-hw1-ex2.txt');
 
 readStream.pipe(csvConverter).pipe(writeStream);
