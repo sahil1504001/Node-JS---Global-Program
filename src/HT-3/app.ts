320010e5fa9dcd38cbe8807b0185485df3a24d90
sahil1504001 <sahilshikalgar001@gmail.com>
2021-05-30 18:32:55 +0530

    Implemented CORS for another hosted domains

--- src/HT-3/app.ts
+++ src/HT-3/app.ts
@@ -7,6 +7,7 @@ import morgan from 'morgan';
 import { CustomLogger } from './logger/custom-logger';
 import { unhandledRejectionHandler, unhandledErrorHandler } from './handlers/handlers';
 import { checkToken } from './token-manager/tokenManager';
+import cors from 'cors';
 
 const app: Application = express();
 const PORT = process.env.port || 3000;
@@ -19,8 +20,9 @@ app.use(morgan(':method :url :status :response-time ms :reqBody'));
 DbConfig.initDb();
 
 app.use(express.json());
-app.set('x-powered-by', false);
+app.use(cors());
 app.use(checkToken);
+app.set('x-powered-by', false);
 
 app.use('/users', userRouter);
 app.use('/groups', groupRouter);
